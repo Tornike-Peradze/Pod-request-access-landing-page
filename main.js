a3d8cc0ec48af2b7480a8e60283935a546eb4bfb
Tornike-Peradze <t_peradze@cu.edu.ge>
2023-07-25 13:45:18 +0400

    add js functionality

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 const email = document.getElementById("email_id");
 const request = document.getElementById("request-btn");
-const error = document.getElementById("error-p");
+const error = document.getElementById("error");
+const success = document.getElementById("success");
+
+const regexPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+
+request.addEventListener("click", function () {
+  if (regexPattern.test(email.value)) {
+    success.style.display = "block";
+  } else {
+    error.style.display = "block";
+  }
+});
